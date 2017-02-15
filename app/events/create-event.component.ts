5f7fd68dd16efa24054d231027aa4e9ff3229ce6
Tej Sidhu <tej.sidhu@ivanti.com>
2017-02-15 07:57:33 +0000

    initial commit

---
+++ app/events/create-event.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core'
+
+@Component({
+    template: `
+        <h1>New Event</h1>
+        <hr>
+        <div class="col-md-6">
+            <h3>[Create Event Form will go here]</h3>
+            <br />
+            <br />
+            <button type="submit" class="btn btn-primary">Save</button>
+            <button type="button" class="btn btn-primary">Cancel</button>
+        </div>
+    `
+})
+export class CreateEventComponent {
+    
+}
