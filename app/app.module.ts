e2db7fd769483c34d077ed746a4c2263f46643fa
Tej Sidhu <tej.sidhu@ivanti.com>
2017-06-13 07:42:52 +0100

    dependency injection

--- app/app.module.ts
+++ app/app.module.ts
@@ -19,14 +19,15 @@ import {
 import { EventsAppComponent } from './events-app-component'
 import { NavBarComponent } from './nav/navbar.component'
 
-import { ToastrService, 
-    CollapsibleWellComponent 
-} from './common/index'
+import { TOASTR_TOKEN, Toastr } from './common/toastr.service'
+import { CollapsibleWellComponent } from './common/collapsible-well.component'
 
 import { appRoutes} from './routes'
 import { Error404Component } from './errors/404.component'
 
 import { AuthService } from './user/auth.service'
+
+declare let toastr: Toastr
 
 @NgModule({
     imports: [
@@ -50,7 +51,10 @@ import { AuthService } from './user/auth.service'
     ],
     providers: [
         EventService,
-        ToastrService,
+        {
+            provide: TOASTR_TOKEN,
+            useValue: toastr
+        },
         EventRouteActivator,
         {
             provide: 'canDeactivateCreateEvent', 
--- app/common/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './toastr.service'
-export * from './collapsible-well.component'
